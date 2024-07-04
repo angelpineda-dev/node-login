14eba7c4266144f4913421083dfabc1b9602266c
angelpineda-dev <angeljpa95@gmail.com>
2024-07-04 08:47:05 -0600

    product post with image

--- controllers/product.js
+++ controllers/product.js
@@ -1,14 +1,21 @@
 const { request, response } = require('express');
 const Product = require('../models/product');
 
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config(process.env.CLOUDINARY_URL);
+
 async function index(req, res = response) {
 
-    const products = await Product.find({});
 
     try {
+        const products = await Product.find().
+            populate('category').
+            exec();
+
         res.json({
             status: true,
-            products
+            data: products
         })
     } catch (error) {
         res.status(400).json({
@@ -19,15 +26,32 @@ async function index(req, res = response) {
 }
 
 async function create(req, res) {
+    let image = '';
 
     try {
-        const product = new Product(req.body);
-        
-        await product.save()
+
+        if (req.files.image) {
+            const { tempFilePath } = req.files.image;
+            const { secure_url } = await cloudinary.uploader.upload( tempFilePath );
+            console.log(secure_url)
+            image = secure_url;
+        }
+
+        const product = new Product({...req.body, image});
+
+        const isProduct = await product.save();
+
+        if (!isProduct && image !== '') {
+                const urlArr = image.split("/");
+                const file = urlArr[urlArr.length - 1];
+                const [ public_id ] = file.split(".");
+
+                cloudinary.uploader.destroy(public_id);
+        }
 
         res.json({
-            status:true,
-            product
+            status: true,
+            data: product
         });
 
     } catch (error) {
@@ -44,7 +68,7 @@ async function update(req, res) {
 
         res.json({
             status:true,
-            product
+            data: product
         })
     } catch (error) {
         res.status(400).json({
@@ -58,7 +82,7 @@ async function remove(req, res) {
     const { id } = req.params;
 
     try {
-        const product = await Product.findByIdAndUpdate(id, { status: false }, { new: true, runValidators: true });
+        const product = await Product.findByIdAndUpdate(id, { status: false }, { new: true, runValidators: true }) ;
 
         if (!product) {
             return res.status(404).json({
@@ -69,7 +93,7 @@ async function remove(req, res) {
 
         res.json({
             status: true,
-            product
+            data: product
         })
     } catch (error) {
         res.status(400).json({
